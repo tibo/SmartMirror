f1f5ea0bee8d6ce9cef53459c8cb25f006b1814b
Thibaut LE LEVIER <thibaut@lelevier.fr>
2016-09-11 17:50:24 +0200

    use more simple icons

--- js/weather.js
+++ js/weather.js
@@ -5,7 +5,7 @@ function refresh_weather() {
     $.getJSON( "http://api.wunderground.com/api/bbdb5ad11fc237c1/conditions/q/"+location+".json", function( data ) {
       var city = data['current_observation']['display_location']['city'];
       var temp = data['current_observation']['temp_c'];
-      var icon = data['current_observation']['icon_url'];
+      var icon = data['current_observation']['icon_url'].replace('/k/','/i/');
 
       $.getJSON( "http://api.wunderground.com/api/bbdb5ad11fc237c1/forecast/q/"+location+".json", function( data ) {
         var day = data['forecast']['simpleforecast']['forecastday'][0];
