8f108be683a00c861ce8442ab8865d01d50431a1
Thibaut LE LEVIER <thibaut@lelevier.fr>
2016-09-11 17:43:28 +0200

    add forecast

--- js/weather.js
+++ js/weather.js
@@ -6,8 +6,15 @@ function refresh_weather() {
       var city = data['current_observation']['display_location']['city'];
       var temp = data['current_observation']['temp_c'];
       var icon = data['current_observation']['icon_url'];
-      var body = "<small>" + city +"</small><br/><img src='"+icon+"' />&nbsp;" + temp + "&deg;";
-      div.html(body);
+
+      $.getJSON( "http://api.wunderground.com/api/bbdb5ad11fc237c1/forecast/q/"+location+".json", function( data ) {
+        var day = data['forecast']['simpleforecast']['forecastday'][0];
+        var low = day['low']['celsius'];
+        var high = day['high']['celsius'];
+        var body = "<small>" + city +"</small><br/><img src='"+icon+"' />&nbsp;" + temp + "&deg; <small>("+ low + "&deg;/" + high + "&deg;)</small>";
+        div.html(body);
+      });
+
     });
   })
 }
